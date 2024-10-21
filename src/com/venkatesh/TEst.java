e6933c4200c36f827e22dbfae6c0bcd7f6677c7b
venkatesh <venkateshsankranthi26@gmail.com>
2024-10-21 20:03:03 +0530

    commit

---
+++ src/com/venkatesh/TEst.java
@@ -0,0 +1,13 @@
+package com.venkatesh;
+
+public class TEst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int N1=2,n2=1,sum;
+	sum=N1+n2;
+	System.out.println(sum);
+			
+	}
+}
+
---
+++ src/com/venkatesh/User.java
@@ -0,0 +1,10 @@
+package com.venkatesh;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
